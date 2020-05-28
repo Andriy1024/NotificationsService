96e6d8c36c868fdcc64023ea36dd2b38119f498a
Zubyk Andriy <a.zubyk@avidi.tech>
2020-05-28 22:17:30 +0300

    add icon url in order model

--- NotificationService.Interfaces/Models/OrderModel.cs
+++ NotificationService.Interfaces/Models/OrderModel.cs
@@ -4,6 +4,7 @@ namespace NotificationService.Application
 {
     public class OrderModel
     {
+        public string Icon { get; set; } = "http://32d94bad36e3.ngrok.io";
         public string BuyerName { get; set; }
         public string ProductName { get; set; }
         public long Payment { get; set; }
